c1e14d735391d25c1faa550a90b9f358b3a1de3e
Daniel Carter <possiblyuncertain@gmail.com>
2021-02-22 17:32:25 +1100

    Add clickable button styling.

--- src/components/SimulationControls.js
+++ src/components/SimulationControls.js
@@ -5,14 +5,14 @@ import '../styles.scss';
 export default function SimulationControls (props) {
   return (
     <div className="spaced-across">
-      <button className="item" onClick={props.togglePlay}>
+      <button className="item control-button" onClick={props.togglePlay}>
         { props.playing ?
           <i className="fa fa-pause" />
           : <i className="fa fa-play" />
         }
       </button>
-      <button className="item" onClick={props.step}><i className="fa fa-step-forward" /></button>
-      <button className="item" onClick={props.reset}><i className="fa fa-step-backward" /></button>
+      <button className="item control-button" onClick={props.step}><i className="fa fa-step-forward" /></button>
+      <button className="item control-button" onClick={props.reset}><i className="fa fa-step-backward" /></button>
     </div>
   );
 }
